ee1e0e49bedb85ad665a6b94ec1d2d1600472a4b
roy-shreya <shreya.roy.blr@gmail.com>
2021-07-19 18:52:07 +0530

    view completed

--- contacts.php
+++ contacts.php
@@ -20,7 +20,7 @@
                                 <th>Account Number </th>
                                 <th>Email-id </th>
                                 <th>Balance </th>
-                                <th>Operation</th>
+                                <th colspan=2>Operation</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -32,16 +32,60 @@
                             ?>
                             <tr>
                                 <td><?php echo $res['name']?></td>
-                                <td><?php echo $res['AccountNo']?></td>                                                
+                                <td class="ano"><?php echo $res['AccountNo']?></td>                                                
                                 <td><?php echo $res['Email']?></td>
                                 <td><?php echo $res['Balance']?></td>
+                                <td><a class=" btn btn-dark btn-sm view" href="#">View</a></td>
                                 <td><a class=" btn btn-dark btn-sm" href="transfer.php?id=<?php echo $res['AccountNo']; ?>">Transfer</a></td>
                             </tr>
+                            <!-- Modal -->
+                            <div class="modal fade" id="customerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Details: </h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="details">
+
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             <?php } ?>
                         </tbody>
-                    </table>
-                </div>  
+                    </table>  
+                </div>
             </div>
         </div>
+        <script type='text/javascript'>
+            $(document).ready(function(){
+                $('.view').click(function (e){
+                    e.preventDefault();
+                    var cust_ano = $(this).closest('tr').find('.ano').text();
+
+                    $.ajax({
+                        type: "POST",
+                        url: "view.php",
+                        data: {
+                            "checking_viewbtn": true,
+                            "customer_ano": cust_ano
+                        },
+                        success: function(response){
+                            $('.details').html(response)
+                            $('#customerModal') .modal('show');
+
+                        }
+                    })
+                })
+            });
+        </script>
     </body>
 </html>
---
+++ view.php
@@ -0,0 +1,22 @@
+<?php
+$user = 'root';
+$pass = '';
+$server = 'localhost';
+$db  = 'bankingsystem';
+
+$conn = mysqli_connect($server, $user, $pass, $db) or die("No connection".mysqli_connect_error());
+if(isset($_POST['checking_viewbtn'])){
+    $ano = $_POST['customer_ano'];
+
+    $selectquery = "select * from customers where AccountNo='$ano' ";
+    $query = mysqli_query($conn, $selectquery);
+    while($res = mysqli_fetch_array($query)){
+        echo $return = '
+            <h3>'.$res['name'].'</h3>
+            <p>Account Number: '.$res['AccountNo'].'</p>
+            <p>Email ID: '.$res['Email'].'</p>
+            <p>Balance: '.$res['Balance'].'</p>
+        ';
+    } 
+}
+?>
